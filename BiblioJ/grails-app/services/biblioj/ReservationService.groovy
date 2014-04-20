7ab7f7c5dcec7da47c133f54f5454071c5cf1862
Guillaume Gurfinkiel <pogonax@hotmail.fr>
2014-04-20 14:00:44 +0200

    service réservation

--- BiblioJ/grails-app/services/biblioj/ReservationService.groovy
+++ BiblioJ/grails-app/services/biblioj/ReservationService.groovy
@@ -3,18 +3,47 @@ package biblioj
 class ReservationService {
 
     def verrifierDiponnibilité() {
-		
+		def listLivreNonDisponible = []
+		session.panier.each { livre ->
+			if (livre.getNombreExemplairesDisponibles() == 0) {
+				listLivreNonDisponible.add(livre.getTitre())
+			}
+		}
+		return listLivreNonDisponible
 	}
 	
 	def validerReservation() {
-		
+		/* suppression des livres non dispo du panier */
+		listLivreNonDisponible = verrifierDiponnibilité()
+		listLivreNonDisponible.each { titreLivre ->
+			session.panier.remove(titreLivre)
+		}
+		/* validation du panier */
+		def dateResa = new Date("yyyy-MM-dd hh:mm:ss")
+		def membre = Membre().findByLogin(session.user)
+		def code = dateResa.toString() + membre.login
+		def resa =  new Reservation(code: code, reservation: dateResa, membre: membre).save()
+		session.panier.each { titreLivre ->
+			new ReservationLivre(reservation: resa, livre: Livre().findByTitre(livre)).save()
+		}
 	}
 	
-	def supprimerReservation() {
-		
+	def supprimerReservation(def code) {
+		def resa = Reservation.findByCode(code);
+		resa.reservationLivres.each { resaLivre ->
+			resaLivre.delete()
+		}
+		resa.delete()
 	}
 	
-	def rendreEmprunt() {
-		
+	def rendreLivre(def codeResa, def titreLivre) {
+		def resa = Reservation.findByCode(codeResa);
+		resa.reservationLivres { resaLivre
+			if (resaLivre.livre.titre.equals(titreLivre)) {
+				resaLivre.delete()
+			}
+		}
+		if (resa.reservationLivres.list().size == 0)
+			resa.delete()
 	}
 }
